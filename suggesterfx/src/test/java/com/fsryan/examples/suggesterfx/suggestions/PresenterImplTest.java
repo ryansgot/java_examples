bfbc286b87923b84cf93f70d1d3de3fef1dc4834
Ryan Scott <ryan@rydora.apt.com>
2016-06-11 17:11:53 -0500

    remove unused import

--- suggesterfx/src/test/java/com/fsryan/examples/suggesterfx/suggestions/PresenterImplTest.java
+++ suggesterfx/src/test/java/com/fsryan/examples/suggesterfx/suggestions/PresenterImplTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
